01ebc5cfb38c3cbc93928f4da23073c46d773f16
xinqi <xinqi@xinqideMacBook-Pro.local>
2024-07-08 14:53:34 +0800

    add multi control inference

--- controlnet_union_test_multi_control.py
+++ controlnet_union_test_multi_control.py
@@ -5,16 +5,11 @@ import random
 import numpy as np
 from PIL import Image
 from diffusers import AutoencoderKL
-from controlnet_aux import MidasDetector, ZoeDetector
+from controlnet_aux import OpenposeDetector, MidasDetector, ZoeDetector
 from diffusers import EulerAncestralDiscreteScheduler
 from models.controlnet_union import ControlNetModel_Union
 from pipeline.pipeline_controlnet_union_sd_xl import StableDiffusionXLControlNetUnionPipeline
 
-
-
-
-prompt = "your prompt, the longer the better, you can describe it as detail as possible"
-negative_prompt = 'longbody, lowres, bad anatomy, bad hands, missing fingers, extra digit, fewer digits, cropped, worst quality, low quality'
 
 
 device=torch.device('cuda:0')
@@ -35,33 +30,38 @@ pipe = StableDiffusionXLControlNetUnionPipeline.from_pretrained(
 
 pipe = pipe.to(device)
 
-processor_zoe = ZoeDetector.from_pretrained("lllyasviel/Annotators")
-processor_midas = MidasDetector.from_pretrained("lllyasviel/Annotators")
+# Example to show(openpose + depth)
+processor_pose = OpenposeDetector.from_pretrained('lllyasviel/ControlNet').to(device)
+processor_zoe = ZoeDetector.from_pretrained("lllyasviel/Annotators").to(device)
+processor_midas = MidasDetector.from_pretrained("lllyasviel/Annotators").to(device)
 
 
 prompt = "your prompt, the longer the better, you can describe it as detail as possible"
 negative_prompt = 'longbody, lowres, bad anatomy, bad hands, missing fingers, extra digit, fewer digits, cropped, worst quality, low quality'
 
 
-controlnet_img = cv2.imread("your image path")
-
+source_img = cv2.imread("your image path")
+controlnet_img_pose = processor_pose(source_img, hand_and_face=False, output_type='cv2')
+# Or you can choose another img for depth detection, the content should be compatible with the pose skeleton.
 if random.random() > 0.5:
-    controlnet_img = processor_zoe(controlnet_img, output_type='cv2')
+    controlnet_img_depth = processor_zoe(source_img, output_type='cv2')
 else:
-    controlnet_img = processor_midas(controlnet_img, output_type='cv2')
+    controlnet_img_depth = processor_midas(source_img, output_type='cv2')
 
 
 # need to resize the image resolution to 1024 * 1024 or same bucket resolution to get the best performance
-height, width, _  = controlnet_img.shape
+height, width, _  = controlnet_img_pose.shape
 ratio = np.sqrt(1024. * 1024. / (width * height))
 new_width, new_height = int(width * ratio), int(height * ratio)
-controlnet_img = cv2.resize(controlnet_img, (new_width, new_height))
-controlnet_img = Image.fromarray(controlnet_img)
+
+controlnet_img_pose = cv2.resize(controlnet_img_pose, (new_width, new_height))
+controlnet_img_depth = cv2.resize(controlnet_img_depth, (new_width, new_height))
+controlnet_img_pose = Image.fromarray(controlnet_img_pose)
+controlnet_img_depth = Image.fromarray(controlnet_img_depth)
 
 
 seed = random.randint(0, 2147483647)
 generator = torch.Generator('cuda').manual_seed(seed)
-
 
 # 0 -- openpose
 # 1 -- depth
@@ -69,15 +69,16 @@ generator = torch.Generator('cuda').manual_seed(seed)
 # 3 -- canny/lineart/anime_lineart/mlsd
 # 4 -- normal
 # 5 -- segment
+# image_list must be compatible with union_control_type
 images = pipe(prompt=[prompt]*1,
-            image_list=[0, controlnet_img, 0, 0, 0, 0], 
+            image_list=[controlnet_img_pose, controlnet_img_depth, 0, 0, 0, 0], 
             negative_prompt=[negative_prompt]*1,
             generator=generator,
-            width=new_width, 
-            height=new_height,
+            width=width, 
+            height=height,
             num_inference_steps=30,
             union_control=True,
-            union_control_type=torch.Tensor([0, 1, 0, 0, 0, 0]),
+            union_control_type=torch.Tensor([1, 1, 0, 0, 0, 0]),
             ).images
 
 images[0].save(f"your image save path, png format is usually better than jpg or webp in terms of image quality but got much bigger")
